ca99c9d41db337489d3893da891b5d74acdf4be6
John Smith <example@stanford.edu>
2013-07-10 08:01:56 +0000

    Edited web.js

--- web.js
+++ web.js
@@ -4,6 +4,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
 //  response.send('Hello World2!');
+  var fs = require('fs');
   response.send(fs.readFile('index.html', function (err, data) {
     if (err) throw err;
     console.log(data);
